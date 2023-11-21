68bd607bb1b0470874a04fa3c3023e694a2622e4
mohajer.hos <mohajer.hos@gmail.com>
2023-11-21 14:52:09 +0000

    backup_srcipt

---
+++ backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+END=`docker ps --format "{{.Names}}"  | wc -l`
+for (( i=1; i<=$END; i++ ))
+do
+	name=`docker ps --format "{{.Names}}" | head -n $i | tail -1`
+	echo $name
+	IMAGE="mohajerhos/backups:$name-`date +%Y-%m-%d_%H-%M`"
+	echo $IMAGE
+	BACKUP="/root/$name-backup-`date +%Y-%m-%d_%H-%M`.tar"
+	echo $BACKUP
+	docker commit $name $IMAGE
+	docker save $IMAGE > $BACKUP
+	docker push $IMAGE
+done
+find ~ -name "*-backup-*.tar" -mtime +7 -exec rm -rf {} \;
+
